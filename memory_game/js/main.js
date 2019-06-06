f1408f879f41f5b54672aad0a089798fdaba62cc
Balthazar Ely <balthazarely@Balthazars-MacBook-Pro.local>
2019-06-05 19:14:28 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,26 @@
 const cards = ["queen", "queen", "king", "king"];
-var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
+const cardsInPlay = [];
 
-if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("you found a match");
-} else {
-	alert("Sorry, try again");
-}
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match");
+	} else {
+		console.log("Sorry, try again.");
+	}
+};
+
+function flipCard(cardId) {
+	console.log("user flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length == 2) {
+		checkForMatch();
+	}
+};
 
 
-console.log("User flipped" + cardOne);
-console.log("User flipped" + cardTwo);
+flipCard(0);
+flipCard(2);
+
+
 
